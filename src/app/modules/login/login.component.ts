0d865c18a81438d8cd063f45148a462876f1d98a
rupesh94 <shahrupesh300@gmail.com>
2020-02-14 17:00:36 +0530

    login page update

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userName: string;
+  password: string;
   constructor() { }
 
   ngOnInit() {
   }
 
+
+
+  loginUser(){
+    if(this.userName == 'admin' && this.password == 'admin123')
+    {
+      console.log('Welcome to Project Tracker System...');
+    }
+  }
 }
