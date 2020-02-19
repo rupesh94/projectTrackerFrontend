eaca45ff001f92e03bf9a1dea54a1c8f73b6f3d9
nishita-khandelwal <nishita.khandelwal007@gmail.com>
2020-02-19 14:43:32 +0530

    initial commit

--- src/app/modules/issue-details/issue-details.component.ts
+++ src/app/modules/issue-details/issue-details.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-issue-details',
   templateUrl: './issue-details.component.html',
   styleUrls: ['./issue-details.component.css']
 })
-export class IssueDetailsComponent implements OnInit {
-  isShow=false;
 
-  toggleDisplay(){
+export class IssueDetailsComponent implements OnInit {
+  isShow = false;
+
+  toggleDisplay() {
     this.isShow = !this.isShow;
   }
-  
-
   constructor() { }
 
   ngOnInit() {
